4b92067b1f2fd0750e02e5eb90923db958616c79
OpenIoTivity <mashraf@openiotivity.com>
2020-05-26 18:47:59 -0700

    Added industry compliance metric.  Names more friendly

--- StockCompliance_py/StockCompliance.py
+++ StockCompliance_py/StockCompliance.py
@@ -1,13 +1,21 @@
+# %load StockCompliance_py\StockCompliance.py
 import requests
 import pandas as pd
+import csv
 
-apikey = f"apikey=<Log into https://financialmodelingprep.com/developer/docs/. Get Api Key>" #### ****
-urlStr = "https://financialmodelingprep.com/api/v3/financials/"
-urlEvStr = "https://financialmodelingprep.com/api/v3/enterprise-values/"
-urlProfStr = "https://financialmodelingprep.com/api/v3/profile/"
+apikey = f"apikey=<Log into https://financialmodelingprep.com/developer/docs/. Get Api Key>" #### ***
+urlStr = "https://financialmodelingprep.com/api/v3/"
+urlEvStr = f"{urlStr}enterprise-values/"
+urlProfStr = f"{urlStr}profile/"
 period = "period=quarter"
 params = f"?{period}&{apikey}"
 paramsProfile = f"?{apikey}"
+icfile = '../industrycompliant.csv'
+icdict = {}
+
+with open(icfile, mode='r') as infile:
+    reader = csv.reader(infile)
+    icdict = {rows[0]: rows[1] for rows in reader}
 
 
 class StockCompliantClass(object):
@@ -15,15 +23,17 @@ class StockCompliantClass(object):
     def __init__(self, symbol):
         self.scdict = dict()
         self.cmdict = dict()
-        balance_sheet_data = requests.get(f"{urlStr}balance-sheet-statement/{symbol}{params}").json()
+        self.icdict = dict()
+        balance_sheet_data = requests.get(
+            f"{urlStr}financials/balance-sheet-statement/{symbol}{params}").json()
         self.balance_sheet = balance_sheet_data['financials'][0]
-        income_data = requests.get(f"{urlStr}income-statement/{symbol}{params}").json()
+        income_data = requests.get(
+            f"{urlStr}financials/income-statement/{symbol}{params}").json()
         self.income = income_data['financials'][0]
         enterp_vals_data = requests.get(f"{urlEvStr}{symbol}{params}").json()
         self.enterp_vals = enterp_vals_data[0]
         profile_data = requests.get(f"{urlProfStr}{symbol}{params}").json()
         self.profile = profile_data[0]
-
 
     def getstockvalues(self):
         self.scdict["Filing Date"] = self.balance_sheet['date']
@@ -40,43 +50,46 @@ class StockCompliantClass(object):
             "Cash and Short Term Investments"] + self.scdict["Receivables"]
         self.scdict["Total Assets"] = float(self.balance_sheet['Total assets'])
         self.scdict["Total Debt"] = float(self.balance_sheet['Total debt'])
-        self.scdict["total_illiquid_assets"] = (
+        self.scdict["Total Illiquid Assets"] = (
             self.scdict["Total Assets"] - self.scdict["Total Liquid Assets"])
         self.scdict["Number of Shares"] = float(
             self.enterp_vals['numberOfShares'])
-        self.scdict["net_liquid_assets_per_share"] = (
+        self.scdict["Net Liquid Assets Per Share"] = (
             self.scdict["Total Assets"] - self.scdict["Receivables"] -
-            self.scdict["Total Liquid Assets"]) - self.scdict[
+            self.scdict["Total Liquid Assets"]) / self.scdict[
             "Number of Shares"]
-        self.scdict["intt_bearing_debt_to_asset_pct"] = self.scdict["Total Debt"] / \
+        self.scdict["Interest Bearing Debt To Asset Pct"] = self.scdict["Total Debt"] / \
             self.scdict["Total Assets"]
-        self.scdict["illiquid_assets_to_assets_pct"] = self.scdict["Total Liquid Assets"] / \
+        self.scdict["Illiquid Assets To Assets Pct"] = self.scdict["Total Illiquid Assets"] / \
             self.scdict["Total Assets"]
-        self.scdict["total_current_assets"] = float(
-            self.balance_sheet['Total current assets'])
-        self.scdict["total_liabilities"] = float(
-            self.balance_sheet['Total liabilities'])
-        self.scdict["revenue"] = float(self.income['Revenue'])
-        self.scdict["interest_expense"] = float(
+        self.scdict["Revenue"] = float(self.income['Revenue'])
+        self.scdict["Interest Expense"] = float(
             self.income['Interest Expense'])
         self.scdict["Stock Price"] = float(self.enterp_vals['stockPrice'])
-        self.scdict["investments_to_total_assets_pct"] = self.scdict["Cash and Cash Equivalents"] / self.scdict[
+        self.scdict["Investments To Total Assets Pct"] = self.scdict["Cash and Cash Equivalents"] / self.scdict[
             "Total Assets"]
-        self.scdict["income_to_total_revenue_pct"] = self.scdict["interest_expense"] / \
-            self.scdict["revenue"]
+        self.scdict["Interest To Total Revenue Pct"] = self.scdict["Interest Expense"] / \
+            self.scdict["Revenue"]
         return self.scdict
 
-    def getcompliancemetrics(self):
-        self.cmdict["intt_bearing_debt_to_asset_compl"] = self.scdict["intt_bearing_debt_to_asset_pct"] < 0.37
-        self.cmdict["illiquid_assets_to_assets_compl"] = self.scdict["illiquid_assets_to_assets_pct"] > 0.25
-        self.cmdict["investments_to_total_assets_compl"] = self.scdict["investments_to_total_assets_pct"] < 0.33
-        self.cmdict["income_to_total_revenue_compl"] = self.scdict["income_to_total_revenue_pct"] < 0.05
-        self.cmdict["net_liquidassets_to_marketprice_compl"] = self.scdict["Stock Price"] > self.scdict[
-            "net_liquid_assets_per_share"]
-        self.cmdict["Overall Compliance"] = self.cmdict["intt_bearing_debt_to_asset_compl"] == True and self.cmdict[
-            "illiquid_assets_to_assets_compl"] == True and self.cmdict["investments_to_total_assets_compl"] == True and \
-            self.cmdict["income_to_total_revenue_compl"] == True and self.cmdict[
-            "net_liquidassets_to_marketprice_compl"] == True
+    def getcompliancemetrics(self, symbol):
+        self.cmdict["Interest Bearing Debt To Asset  Compl"] = self.scdict["Interest Bearing Debt To Asset Pct"] < 0.37
+        self.cmdict["Illiquid Assets To Assets Compl"] = self.scdict["Illiquid Assets To Assets Pct"] > 0.25
+        self.cmdict["Investments To Total Assets Compl"] = self.scdict["Investments To Total Assets Pct"] < 0.33
+        self.cmdict["Income To Total Revenue Compl"] = self.scdict["Interest To Total Revenue Pct"] < 0.05
+        self.cmdict["Net Liquid Assets To Price Compl"] = self.scdict["Stock Price"] > self.scdict[
+            "Net Liquid Assets Per Share"]
+        self.cmdict["Industry Compliance"] = "Yes"
+        if (icdict[symbol] == "N"):
+            self.cmdict["Industry Compliance"] = "No"
+
+        if (self.cmdict["Interest Bearing Debt To Asset  Compl"] == True and self.cmdict[
+            "Illiquid Assets To Assets Compl"] == True and self.cmdict["Investments To Total Assets Compl"] == True and
+            self.cmdict["Income To Total Revenue Compl"] == True and self.cmdict[
+                "Net Liquid Assets To Price Compl"] == True and self.cmdict["Industry Compliance"] == "Yes"):
+            self.cmdict["Overall Compliance"] = "Yes"
+        else:
+            self.cmdict["Overall Compliance"] = "No"
         return self.cmdict
 
 
@@ -85,7 +98,7 @@ symbolList = symbolList.replace(' ', '')
 for symbol in symbolList.split(","):
     scinstance = StockCompliantClass(symbol)
     scdict = scinstance.getstockvalues()
-    cmdict = scinstance.getcompliancemetrics()
+    cmdict = scinstance.getcompliancemetrics(symbol)
     result = "Yes"
     if (cmdict["Overall Compliance"] == False):
         result = "No"
@@ -96,4 +109,3 @@ for symbol in symbolList.split(","):
     print("** Compliance Metrics")
     for name, value in cmdict.items():
         print("  ", name, "=", value)
-
